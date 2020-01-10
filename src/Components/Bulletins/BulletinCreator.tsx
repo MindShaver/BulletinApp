9566b3c71ca337846ca1892545348c27b13f9da9
Tara Livingston <Tara@Taras-MacBook-Pro.local>
2020-01-10 09:46:02 -0600

    User can specify title for new bulletins

--- src/Components/Bulletins/BulletinCreator.tsx
+++ src/Components/Bulletins/BulletinCreator.tsx
@@ -1,5 +1,5 @@
 import IBulletinData from "../../Services/IBulletinData";
-import React from "react";
+import React, { useState } from "react";
 import uuid from "uuid";
 
 export interface IBulletinCreatorProps {
@@ -7,15 +7,30 @@ export interface IBulletinCreatorProps {
 }
 
 const BulletinCreator: React.FC<IBulletinCreatorProps> = props => {
-  const submit = () => {
+  const [title, setTitle] = useState("");
+
+  const submit = (evt: any) => {
+    evt.preventDefault();
     const bulletin: IBulletinData = {
       id: uuid.v4(),
-      title: "sdfoij",
+      title: title,
       description: "sdfj",
       votes: 2
     };
     props.handleSubmit(bulletin);
   };
-  return <button onClick={submit}>Create Bulletin</button>;
+  return (
+    <form onSubmit={submit}>
+      <label>
+        Bulletin Title:
+        <input
+          type="text"
+          value={title}
+          onChange={e => setTitle(e.target.value)}
+        />
+      </label>
+      <input type="submit" value="Submit" />
+    </form>
+  );
 };
 export default BulletinCreator;
